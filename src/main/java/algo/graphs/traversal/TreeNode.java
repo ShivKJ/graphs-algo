b9b8e67b4163cd557323f993829370eb007e7c8a
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-02-24 09:15:24 +0530

    removing files for binary tree

--- src/main/java/algo/graphs/traversal/TreeNode.java
+++
@@ -1,5 +0,0 @@
-package algo.graphs.traversal;
-
-public abstract class TreeNode extends TraversalVertex {
-	public abstract double parentEdge();
-}
--- src/main/java/algo/graphs/tree/BinaryTree.java
+++
@@ -1,6 +0,0 @@
-package algo.graphs.tree;
-
-import algo.graphs.Edge;
-import algo.graphs.traversal.BinaryTreeNode;
-
-public interface BinaryTree<V extends BinaryTreeNode, W extends Edge<? extends V>> extends Tree<V, W> {}
